e651ce4c2b74991cf09dd3e9a343062004a72c2c
F0xedb <tommeyers.tm127@gmail.com>
2019-06-29 21:18:45 +0200

    added tos as a binary tool

--- keepalive.sh
+++ keepalive.sh
@@ -3,15 +3,23 @@
 while true
 do
     live=$(ps -ax | grep polybar | awk '{print $6}')
+    bIsMain=False
+    bIsWorkspace=False
     for item in $live
     do
         if [ "$item" == "main" ]; then
-            nohup polybar main &> /dev/null &
+                bIsMain=True
         fi
 
         if [ "$item" == "workspaces" ];then
-            nohup polybar workspace &> /dev/null &
+                bIsWorkspace=True
         fi
     done
+    if [ "$bIsMain" == False ]; then
+            nohup polybar main &>/dev/null &
+    fi
+    if [ "$bIsWorkspace" == False ]; then
+            nohup polybar workspaces &> /dev/null &
+    fi
     sleep 5
 done
