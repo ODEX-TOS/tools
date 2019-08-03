42ed825059f757eb27f3fb91ccb9d94daeeb8a0a
Tom Meyers <tommeyers.tm127@gmail.com>
2019-08-03 14:59:46 +0000

    Fixed keepalive to support wayland

--- keepalive.sh
+++ keepalive.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-while true
-do
+function xorg {
     live=$(ps -ax | grep polybar | awk '{print $6}')
     bIsMain=False
     bIsWorkspace=False
@@ -20,6 +18,34 @@ do
     fi
     if [ "$bIsWorkspace" == False ]; then
             nohup polybar workspaces &> /dev/null &
+    fi
+
+}
+
+function wayland { 
+    live=$(ps -ax | grep waybar | awk '{print $5}')
+    bIsAlive=False
+    for item in $live
+    do
+        echo $item
+        if [ "$item" == "waybar" ]; then
+                bIsAlive=True
+        fi
+    done
+    if [ "$bIsAlive" == False ]; then
+            nohup waybar &>/dev/null &
+    fi
+}
+
+while true
+do
+    display=$(ps -ax | grep sway)
+    display=$(echo $display | awk '{print $5}')
+    echo $display
+    if [[ "$display" == "sway" ]]; then
+            wayland
+    else
+            xorg
     fi
     sleep 5
 done
