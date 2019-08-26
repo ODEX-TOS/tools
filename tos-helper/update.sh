d50a9b25019cafe754e14a44f2da2910f9d479b2
Tom <tom@odex.be>
2019-08-26 01:04:01 +0000

    Update update.sh

--- tos-helper/update.sh
+++ tos-helper/update.sh
@@ -86,7 +86,7 @@ function update {
     if [[ ! "$(command -v neofetch)" ]]; then
         yay -Syu neofetch
     fi
-    sudo sh -c 'curl https://raw.githubusercontent.com/F0xedb/tos-live/master/toslive/version-edit.txt > /etc/version'
+    sudo sh -c 'curl https://raw.githubusercontent.com/ODEX-TOS/tos-live/master/toslive/version-edit.txt > /etc/version'
 }
 
 function repair {
