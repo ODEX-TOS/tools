4b4740131e7d5242703bca8455df4d4b3ac971c0
F0xedb <tom@odex.be>
2019-09-04 21:30:14 +0200

    Added new installer to tos, old is still present
    We have added the cli based installer to tos

--- tos-helper/installer.sh
+++ tos-helper/installer.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-source tos-helper/color.sh
-
 
 function installtosdialog {
-    printf "${RED}Installing arch via a dialog${NC}\n"
+    printf "${RED}Installing tos${NC}\n"
     if [[ ! -f dialogarchinstall ]]; then
         curl https://raw.githubusercontent.com/ODEX-TOS/tools/master/tosinstall -o tosinstall
         chmod +x tosinstall
     fi
-    ./tosinstall 
+    if [[ ! -d $HOME/api ]]; then
+            git clone https://github.com/ODEX-TOS/tos-installer-backend $HOME/api
+    fi
+    if [[ ! -d $HOME/cli ]]; then
+            git clone https://github.com/ODEX-TOS/installer-curses $HOME/cli
+    fi
+    bash $HOME/cli/install.sh || exit 1
+    python3 $HOME/api/main.py --in gen.yaml --out run.sh || exit 1
+    bash run.sh
 }
 case "$1" in
     "-iso")
