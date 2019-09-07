bee55f91b3e0e54e85bafa2b8a8755586a222641
F0xedb <tom@odex.be>
2019-09-07 15:52:19 +0200

    Don't clone the sources but install the backend through the repo

--- tos-helper/installer.sh
+++ tos-helper/installer.sh
@@ -6,14 +6,14 @@ function installtosdialog() {
 		curl https://raw.githubusercontent.com/ODEX-TOS/tools/master/tosinstall -o tosinstall
 		chmod +x tosinstall
 	fi
-	if [[ ! -d "$HOME"/api ]]; then
-		git clone https://github.com/ODEX-TOS/tos-installer-backend "$HOME"/api
+    if [[ ! "$(command -v os-install)" ]]; then
+            pacman -Syu installer-backend --noconfirm
 	fi
 	if [[ ! -d "$HOME"/cli ]]; then
 		git clone https://github.com/ODEX-TOS/installer-curses "$HOME"/cli
 	fi
 	bash "$HOME"/cli/install.sh || exit 1
-	python3 "$HOME"/api/main.py --in gen.yaml --out run.sh || exit 1
+	os-install --in gen.yaml --out run.sh || exit 1
 	bash run.sh
 }
 case "$1" in
