33b1f5f1195603135ba18a95ab6f52cc99544f03
F0xedb <tom@odex.be>
2019-10-17 17:47:02 +0200

    Added new bluetooth settings debug

--- tos-helper/bluetooth.sh
+++ tos-helper/bluetooth.sh
@@ -20,8 +20,7 @@ case "$2" in
                 touch "$themefile"
                 printf "off\ntime=1000\nbluetooth=false\n" >>"$themefile"
             fi
-            sed -i 's/^bluetooth=false$/'"bluetooth=$1"'/' "$themefile"
-            sed -i 's/^bluetooth=true$/'"bluetooth=$1"'/' "$themefile"
+            sed -i 's/^bluetooth=.*$/'"bluetooth=$1"'/' "$themefile"
     ;;
    "l"|"list")
             if [[ $3 == "scan" ]]; then
