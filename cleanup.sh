2c0cbf4c83c77d4639239e206ab410a44fc709cd
Tom Meyers <tom@odex.be>
2020-05-17 18:42:14 +0200

    Set the official wallpaper to the global wallpaper location

--- cleanup.sh
+++ cleanup.sh
@@ -180,7 +180,7 @@ do_user_setup(){
     # set the lightdm theme to the tos theme
     sed  's:#greeter-session=.*$:greeter-session=lightdm-webkit2-greeter:' /etc/lightdm/lightdm.conf
     sed  's:#greeter-session=.*$:greeter-session=lightdm-webkit2-greeter:' /etc/lightdm/lightdm.conf
-    printf "on\ntime=1800\nfull=false\n/home/$NEW_USER/Pictures/drawing/cabin.png" > /home/$NEW_USER/.config/tos/theme
+    printf "on\ntime=1800\nfull=false\n/usr/share/backgrounds/tos/cabin.png" > /home/$NEW_USER/.config/tos/theme
 
     # set awesomewm as the default launcher for lightdm (only if it exists)
     if [[ "$(command -v awesome)" ]]; then
