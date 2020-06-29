9127a60de07b19cd8b8a559a83bd00030617e3c3
Tom Meyers <tom@odex.be>
2020-06-29 12:42:46 +0200

    Reposition the removal of .oh-my-zsh to the latest possible moment

--- cleanup.sh
+++ cleanup.sh
@@ -130,10 +130,10 @@ do_user_setup(){
 
     yay -Syu --noconfirm zsh
     sudo chsh $NEW_USER -s /bin/zsh
-    rm -rf /home/$NEW_USER/.oh-my.zsh
     curl https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -o install.sh
     export RUNZSH=no
     export ZSH=/home/$NEW_USER/.oh-my-zsh
+    rm -rf /home/$NEW_USER/.oh-my.zsh
     sh install.sh
     rm install.sh
     rm /home/$NEW_USER/.zshrc /home/$NEW_USER/.vimrc /home/$NEW_USER/.profile
