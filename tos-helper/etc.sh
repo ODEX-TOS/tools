28d5c93d6143e7342986dac2979e17c3976ce9ab
Tom Meyers <tom@odex.be>
2020-12-27 22:31:20 +0100

    TOS: added a new info option
    The new info option gives a bit of information about the system
    This is a quick and easy way to gather information about why a system
    might be failing

--- tos-helper/etc.sh
+++ tos-helper/etc.sh
@@ -23,6 +23,45 @@
 # SOFTWARE.
 
 # shellcheck disable=SC2059
+source tos-helper/color.sh
+
+function info {
+	status="${GREEN}healthy${NC}"
+	if [[ "$(find ~/.cache/tde/ -amin -30 -iname error.log | wc -l)" -gt "0" ]]; then
+		status="${RED}unhealthy${NC}"
+		unhealthy="${RED}HEALTH STATUS:${NC}"
+	fi
+	updates="$(checkupdates-tos | sed ':a;N;$!ba;s/\n/---___---/g')"
+	if [[ "$(echo -e "$updates" | wc -l)" -gt "0" ]]; then
+		status="${RED}unhealthy${NC}"
+		unhealthy="${RED}HEALTH STATUS:${NC}"
+	fi
+
+	echo -e "${ORANGE}STATUS:${NC} \t\t\t\t $status"
+	echo ""
+	echo -e "${ORANGE}TDE:${NC}"
+	echo -e "\tVersion: \t\t\t $(tde-client "return awesome.version" | awk '{print $2}' | tr -d '"')"
+	echo -e "\tType: \t\t\t\t $(tde-client "return awesome.release" | awk '{print $2}' | tr -d '"')"
+	echo -e "\tRelease: \t\t\t $(tde-client "return require('release')" | awk '{print $2}' | tr -d '"')"
+	echo -e "\tCPU usage: \t\t\t $(ps -aux | grep $(pgrep tde) | head -n1 | awk '{print $3"%"}')"
+	echo ""
+	echo -e "${ORANGE}TOS:${NC}"
+	echo -e "\tVersion: \t\t\t $(cat /etc/version)"
+	echo -e "\tPackages: \t\t\t $(pacman -Q | wc -l)"
+	echo -e "\tUptime: \t\t\t $(uptime -p)"
+	echo -e "\tKernel: \t\t\t $(uname -r)"
+
+	if [[ -n "$unhealthy" ]]; then
+		echo ""
+		echo -e "$unhealthy"
+		/bin/cat ~/.cache/tde/error.log | tail -n20 | awk '{print "\tINFO: \t\t\t\t", $0}'
+		echo ""
+		while IFS= read -r line
+		do
+			echo -e "\tUPDATES:\t\t\t $line"
+		done <<< "$(echo -e $updates | sed 's/---___---/\n/g')"
+	fi
+}
 
 case "$1" in
     "-c"|"--crypto")
@@ -51,5 +90,8 @@ case "$1" in
         sleep 0.5
     	done
     ;;
+	"i"|"info")
+		info
+	;;
 esac
 
