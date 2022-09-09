5a599b9c3c3dce97e139924b339935118cfb3ce5
F0xedb <tom@odex.be>
2022-09-09 12:11:24 +0200

    Use the newest version of gnome-shell dependencies

--- gnome-setup.sh
+++ gnome-setup.sh
@@ -3,63 +3,26 @@
 # gnome-shell-extension-blur-my-shell
 gnome-extensions enable blur-my-shell@aunetx
 
-# gnome-shell-extension-pop-shell-git
-gnome-extensions enable pop-shell@system76.com 
-
-# gnome-shell-extension-clipboard-indicator
-gnome-extensions enable clipboard-indicator@tudmotu.com
-
-# gnome-shell-extension-net-speed
-gnome-extensions enable netspeed@alynx.one
+# gnome-shell-extension-pano-git
+gnome-extensions enable pano@elhan.io
 
 # gnome-shell-extension-dash-to-dock
-gnome-extensions enable ubuntu-dock@ubuntu.com
+gnome-extensions enable gsconnect@andyholmes.github.io
 
-# gnome-shell-extension-desktop-icons-ng
-#gnome-extensions enable ding@rastersoft.com
+# tos-shell
+gnome-extensions enable tos@odex.be
 
 # TODO: COnfigure keybindings and settings of the extentions
 # settings options are stored in org.gnome.shell.extensions.*
 
 
-
 # Blur-my-shell disable dash-to-dock
 gsettings set org.gnome.shell.extensions.blur-my-shell dash-to-dock-blur "false"
 
-# pop shell
-gsettings set org.gnome.shell.extensions.pop-shell focus-down "['<Super>Down']"
-gsettings set org.gnome.shell.extensions.pop-shell focus-left "['<Super>Left']"
-gsettings set org.gnome.shell.extensions.pop-shell focus-right "['<Super>Right']"
-gsettings set org.gnome.shell.extensions.pop-shell focus-up "['<Super>Up']"
-gsettings set org.gnome.shell.extensions.pop-shell hint-color-rgba 'rgb(206,108,251)'
-gsettings set org.gnome.shell.extensions.pop-shell show-title 'false'
-gsettings set org.gnome.shell.extensions.pop-shell smart-gaps 'true'
-gsettings set org.gnome.shell.extensions.pop-shell snap-to-grid 'true'
-gsettings set org.gnome.shell.extensions.pop-shell active-hint 'false'
-gsettings set org.gnome.shell.extensions.pop-shell tile-by-default 'true'
-gsettings set org.gnome.shell.extensions.pop-shell tile-by-default 'true'
-gsettings set org.gnome.shell.extensions.pop-shell gap-inner '2'
-gsettings set org.gnome.shell.extensions.pop-shell gap-outer '2'
-gsettings set org.gnome.shell.extensions.pop-shell tile-enter "['<Super>backslash']"
-gsettings set org.gnome.shell.extensions.pop-shell toggle-floating "['<Super>c']"
-gsettings set org.gnome.shell.extensions.pop-shell toggle-stacking-global "['<Shift><Control><Alt><Super>s']"
-gsettings set org.gnome.shell.extensions.pop-shell activate-launcher "['<Super>slash']"
-gsettings set org.gnome.shell.extensions.pop-shell toggle-tiling "['<Super>y']"
-gsettings set org.gnome.shell.extensions.pop-shell tile-swap-down "['<Control><Super>Down']" 
-gsettings set org.gnome.shell.extensions.pop-shell tile-swap-left "['<Control><Super>Left']"
-gsettings set org.gnome.shell.extensions.pop-shell tile-swap-right "['<Control><Super>Right']"
-gsettings set org.gnome.shell.extensions.pop-shell tile-swap-up "['<Control><Super>Up']"
+# pano
+gsettings set org.gnome.shell.extensions.pano history-length "50"
+gsettings set org.gnome.shell.extensions.pano shortcut "['<Super>u']"
 
-# dash-to-dock
-gsettings set org.gnome.shell.extensions.dash-to-dock activate-single-window "true"
-gsettings set org.gnome.shell.extensions.dash-to-dock animate-show-apps "true"
-gsettings set org.gnome.shell.extensions.dash-to-dock apply-glossy-effect "true"
-gsettings set org.gnome.shell.extensions.dash-to-dock autohide "true"
-gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity "0.8"
-
-# clipboard-indicator
-gsettings set org.gnome.shell.extensions.clipboard-indicator strip-text "true"
-gsettings set org.gnome.shell.extensions.clipboard-indicator toggle-menu "['<Super>u']"
 
 # setup keybindings
 for i in {1..9}; do
